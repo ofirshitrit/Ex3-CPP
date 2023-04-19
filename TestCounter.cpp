05c3ae059e88f96d08034787f4123775caa18ecd
ofirshitrit <ofirha18@gmail.com>
2023-04-19 10:08:29 +0300

    add namespace ariel

--- TestCounter.cpp
+++ TestCounter.cpp
@@ -21,7 +21,7 @@ struct ReporterCounter: public ConsoleReporter {
 };
 
 REGISTER_REPORTER("counter", 1, ReporterCounter);
-
+//
 //int main(int argc, char** argv) {
 //    Context context;
 //    context.addFilter("reporters", "counter");
--- sources/Fraction.cpp
+++ sources/Fraction.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+namespace ariel {
 
 //
 //Fraction::Fraction(int numer, int dumer) : numerator(numer), denominator(dumer)
@@ -30,134 +31,135 @@ using namespace std;
 
 
 
-ostream &operator<<(ostream &ostr, const Fraction &frac) {
-    ostr << frac.numerator << "/" << frac.denominator;
-    return ostr;
-}
-
-istream &operator>>(istream &input, Fraction &frac) {
-    return input;
-}
-
-Fraction operator+(const Fraction &frac1, const Fraction &frac2) {
-    int numerator = frac1.getNumerator() * frac2.getDenominator() + frac2.getNumerator() * frac1.getDenominator();
-    int denominator = frac1.getDenominator() * frac2.getDenominator();
-    Fraction result(numerator, denominator);
-    result.reduceFraction();
-    return result;
-}
-
-Fraction operator-(const Fraction &frac1, const Fraction &frac2) {
-    int numerator = frac1.getNumerator() * frac2.getDenominator() - frac2.getNumerator() * frac1.getDenominator();
-    int denominator = frac1.getDenominator() * frac2.getDenominator();
-    Fraction result(numerator, denominator);
-    result.reduceFraction();
-    return result;
-}
-
-Fraction operator*(const Fraction &frac1, const Fraction &frac2) {
-    int numerator = frac1.getNumerator() * frac2.getNumerator();
-    int denominator = frac1.getDenominator() * frac2.getDenominator();
-    Fraction result(numerator, denominator);
-    result.reduceFraction();
-    return result;
-}
-
-Fraction operator/(const Fraction &frac1, const Fraction &frac2) {
-    Fraction tmp(frac2);
-    tmp.swapNumerAndDenom();
-    return frac1 * tmp;
-}
-
-bool operator>=(const Fraction &frac1, const Fraction &frac2) {
-    return (frac1.getNumerator() * frac2.getDenominator()) >= (frac2.getNumerator() * frac1.getDenominator());
-
-}
-
-bool operator<=(const Fraction &frac1, const Fraction &frac2) {
-    return (frac1.getNumerator() * frac2.getDenominator()) <= (frac2.getNumerator() * frac1.getDenominator());
-
-}
-
-bool operator>(const Fraction &frac1, const Fraction &frac2) {
-    return (frac1.getNumerator() * frac2.getDenominator()) > (frac2.getNumerator() * frac1.getDenominator());
-
-}
-
-bool operator<(const Fraction &frac1, const Fraction &frac2) {
-    return (frac1.getNumerator() * frac2.getDenominator()) < (frac2.getNumerator() * frac1.getDenominator());
-}
-
-bool operator==(const Fraction &frac1, const Fraction &frac2) {
-    Fraction f1 = frac1;
-    Fraction f2 = frac2;
-    f1.reduceFraction();
-    f2.reduceFraction();
-    return (f1.getDenominator() == f2.getDenominator() && f1.getNumerator() == f2.getNumerator());
-}
-
-bool operator!=(const Fraction &frac1, const Fraction &frac2) {
-    return !(frac1 == frac2);
-}
-
-Fraction &Fraction::operator++() {
-    Fraction tmp = *this;
-    *this = tmp + 1;
-    return *this;
-}
-
-Fraction &Fraction::operator--() {
-    Fraction tmp = *this;
-    *this = tmp - 1;
-    return *this;
-}
-
-Fraction Fraction::operator++(int) {
-    Fraction tmp = *this;
-    *this = tmp + 1;
-    return tmp;
-}
-
-Fraction Fraction::operator--(int) {
-    Fraction tmp = *this;
-    *this = tmp - 1;
-    return tmp;
-}
-
-
-int Fraction::gcd(int a, int b) {
-    if (b == 0) {
-        return a;
-    } else {
-        return gcd(b, a % b);
+    ostream &operator<<(ostream &ostr, const Fraction &frac) {
+        ostr << frac.numerator << "/" << frac.denominator;
+        return ostr;
     }
-}
 
-void Fraction::reduceFraction() {
-    int commonFactor = gcd(this->numerator, this->denominator);
-    numerator /= commonFactor;
-    denominator /= commonFactor;
-}
+    istream &operator>>(istream &input, Fraction &frac) {
+        return input;
+    }
 
-void Fraction::swapNumerAndDenom() {
-    int tmp = this->numerator;
-    this->numerator = this->denominator;
-    this->denominator = tmp;
-}
+    Fraction operator+(const Fraction &frac1, const Fraction &frac2) {
+        int numerator = frac1.getNumerator() * frac2.getDenominator() + frac2.getNumerator() * frac1.getDenominator();
+        int denominator = frac1.getDenominator() * frac2.getDenominator();
+        Fraction result(numerator, denominator);
+        result.reduceFraction();
+        return result;
+    }
+
+    Fraction operator-(const Fraction &frac1, const Fraction &frac2) {
+        int numerator = frac1.getNumerator() * frac2.getDenominator() - frac2.getNumerator() * frac1.getDenominator();
+        int denominator = frac1.getDenominator() * frac2.getDenominator();
+        Fraction result(numerator, denominator);
+        result.reduceFraction();
+        return result;
+    }
+
+    Fraction operator*(const Fraction &frac1, const Fraction &frac2) {
+        int numerator = frac1.getNumerator() * frac2.getNumerator();
+        int denominator = frac1.getDenominator() * frac2.getDenominator();
+        Fraction result(numerator, denominator);
+        result.reduceFraction();
+        return result;
+    }
+
+    Fraction operator/(const Fraction &frac1, const Fraction &frac2) {
+        Fraction tmp(frac2);
+        tmp.swapNumerAndDenom();
+        return frac1 * tmp;
+    }
+
+    bool operator>=(const Fraction &frac1, const Fraction &frac2) {
+        return (frac1.getNumerator() * frac2.getDenominator()) >= (frac2.getNumerator() * frac1.getDenominator());
+
+    }
+
+    bool operator<=(const Fraction &frac1, const Fraction &frac2) {
+        return (frac1.getNumerator() * frac2.getDenominator()) <= (frac2.getNumerator() * frac1.getDenominator());
+
+    }
+
+    bool operator>(const Fraction &frac1, const Fraction &frac2) {
+        return (frac1.getNumerator() * frac2.getDenominator()) > (frac2.getNumerator() * frac1.getDenominator());
+
+    }
+
+    bool operator<(const Fraction &frac1, const Fraction &frac2) {
+        return (frac1.getNumerator() * frac2.getDenominator()) < (frac2.getNumerator() * frac1.getDenominator());
+    }
+
+    bool operator==(const Fraction &frac1, const Fraction &frac2) {
+        Fraction f1 = frac1;
+        Fraction f2 = frac2;
+        f1.reduceFraction();
+        f2.reduceFraction();
+        return (f1.getDenominator() == f2.getDenominator() && f1.getNumerator() == f2.getNumerator());
+    }
+
+    bool operator!=(const Fraction &frac1, const Fraction &frac2) {
+        return !(frac1 == frac2);
+    }
+
+    Fraction &Fraction::operator++() {
+        Fraction tmp = *this;
+        *this = tmp + 1;
+        return *this;
+    }
+
+    Fraction &Fraction::operator--() {
+        Fraction tmp = *this;
+        *this = tmp - 1;
+        return *this;
+    }
+
+    Fraction Fraction::operator++(int) {
+        Fraction tmp = *this;
+        *this = tmp + 1;
+        return tmp;
+    }
+
+    Fraction Fraction::operator--(int) {
+        Fraction tmp = *this;
+        *this = tmp - 1;
+        return tmp;
+    }
 
 
-int Fraction::getNumerator() const {
-    return numerator;
-}
+    int Fraction::gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b);
+        }
+    }
 
-void Fraction::setNumerator(int numerator) {
-    Fraction::numerator = numerator;
-}
+    void Fraction::reduceFraction() {
+        int commonFactor = gcd(this->numerator, this->denominator);
+        numerator /= commonFactor;
+        denominator /= commonFactor;
+    }
 
-int Fraction::getDenominator() const {
-    return denominator;
-}
+    void Fraction::swapNumerAndDenom() {
+        int tmp = this->numerator;
+        this->numerator = this->denominator;
+        this->denominator = tmp;
+    }
 
-void Fraction::setDenominator(int denominator) {
-    Fraction::denominator = denominator;
+
+    int Fraction::getNumerator() const {
+        return numerator;
+    }
+
+    void Fraction::setNumerator(int numerator) {
+        Fraction::numerator = numerator;
+    }
+
+    int Fraction::getDenominator() const {
+        return denominator;
+    }
+
+    void Fraction::setDenominator(int denominator) {
+        Fraction::denominator = denominator;
+    }
 }
